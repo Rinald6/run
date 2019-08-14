af0a84aa531fb31451ac5fa995326bbfcb6a3537
Josh Ambush <37254588+joshambush@users.noreply.github.com>
2019-08-13 21:30:20 -0400

    Update finaly.py

--- finaly.py
+++ finaly.py
@@ -1,24 +1,30 @@
+"""
 import main
 import trippin
 import adv
 import app
 import gamee
 import nowitness
+"""
+import subprocess
 def execute():
-    num=(input("Enter a number to access the file: "))
-    if num==1:
-        main
-    elif num==2:
-        trippin
-    elif num==3:
-        adv
-    elif num==4:
-        app
-    elif num==5:
-        gamee
-    elif num==6:
-        nowitness
-    else:
-        print("Invalid input!")
+    while True:
+        num=int(input("Enter a number to access the file: "))
+        if num==1:
+            subprocess.call("python main.py")
+        elif num==2:
+            subprocess.call("python trippin.py")
+        elif num==3:
+            adv
+            subprocess.call("python adv.py")
+        elif num==4:
+            subprocess.call("python app.py")
+        elif num==5:
+            subprocess.call("python gamee.py")
+        elif num==6:
+            nowitness
+            subprocess.call("python nowitness.py")
+        else:
+            print("Invalid input!")
 
 execute()
